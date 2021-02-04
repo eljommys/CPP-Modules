cd1fc4edc2ae3f3a25e0f960c30ad19c3379bd5e
Jaime Serrano gonzalez <jserrano@c1r10s1.42madrid.com>
2021-02-04 16:02:08 +0100

    hola

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jserrano <jserrano@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 14:53:38 by jserrano          #+#    #+#             */
+/*   Updated: 2021/02/04 15:56:21 by jserrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+int	main(int argc, char const *argv[])
+{
+	if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	return (0);
+}
