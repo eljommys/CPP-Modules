5acbd6ae03883bd8d8227ef40f970b31182303c2
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-02-06 16:38:34 +0100

    finished

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/04 14:53:38 by jserrano          #+#    #+#             */
-/*   Updated: 2021/02/05 15:46:18 by marvin           ###   ########.fr       */
+/*   Created: 2021/02/06 16:34:21 by marvin            #+#    #+#             */
+/*   Updated: 2021/02/06 16:34:21 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,17 @@
 #include <string>
 #include <locale>
 
-using namespace std;
-
 int	main(int argc, char const *argv[]){
 	if (argc == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < argc; i++){
 			std::string str(argv[i]);
 			for (size_t j = 0; j < str.length(); j++)
-				std::cout << (char)std::toupper(str[j]) << std::endl;
+				std::cout << (char)std::toupper(str[j]);
 		}
+		std::cout << std::endl;
 	}
 	return (0);
 }
