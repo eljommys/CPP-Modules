dccbc43a8da4644e0098dceda6551734fc42dbd3
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-02-10 17:50:45 +0100

    make

--- 01/ex07/main.cpp
+++ 01/ex07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 17:48:02 by jserrano          #+#    #+#             */
-/*   Updated: 2021/02/09 22:46:06 by marvin           ###   ########.fr       */
+/*   Updated: 2021/02/10 17:50:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,24 @@ int     main(int argc, char **argv){
         fname << argv[1] << ".replace";
 
         std::string     content;
+        std::string     aux;
         std::ofstream   res (fname.str());
         std::string     s1 = argv[2];
         std::string     s2 = argv[3];
 
-        size_t pos;
+        int pos[2];
         while (getline(file, content)){
-            while ((pos = content.find(s1)) != std::string::npos)
-                content.replace(pos, s1.length(), s2);
-            res << content;
+
+            aux = content;
+            pos[1] = -1;
+
+            while ((pos[0] = content.find(s1)) != (int)std::string::npos && pos[0] != pos[1]){
+
+                aux.replace(pos[0], s1.length(), s2);
+                pos[1] = pos[0];
+            }
+
+            res << aux;
             if (file.eof() == false)
                 res << std::endl;
         }
