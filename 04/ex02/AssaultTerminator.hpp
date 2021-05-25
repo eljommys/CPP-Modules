2658ab9832e86a0e07b8b979f27455495808a4dc
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-05-25 19:30:20 +0200

    default commit message

--- 04/ex02/AssaultTerminator.hpp
+++ 04/ex02/AssaultTerminator.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AssaultTerminator.hpp                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jserrano <jserrano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:30:16 by jserrano          #+#    #+#             */
-/*   Updated: 2021/02/16 13:31:03 by jserrano         ###   ########.fr       */
+/*   Updated: 2021/05/25 19:00:48 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class	AssaultTerminator: public ISpaceMarine{
 
 	public:
 		AssaultTerminator();
-		~AssaultTerminator();
+		virtual ~AssaultTerminator();
 		ISpaceMarine* 	clone() const;
 		void			battleCry() const;
 		void			rangedAttack() const;
--- 04/ex02/main.cpp
+++ 04/ex02/main.cpp
@@ -6,33 +6,13 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 13:36:09 by jserrano          #+#    #+#             */
-/*   Updated: 2021/05/09 19:31:38 by marvin           ###   ########.fr       */
+/*   Updated: 2021/05/25 19:23:18 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AssaultTerminator.hpp"
 #include "TacticalMarine.hpp"
 #include "Squad.hpp"
-
-/* int main(){
-
-    ISpaceMarine* bob = new TacticalMarine;
-    ISpaceMarine* jim = new AssaultTerminator;
-
-    ISquad* vlc = new Squad;
-    vlc->push(bob);
-    vlc->push(jim);
-    for (int i = 0; i < vlc->getCount(); ++i){
-
-        ISpaceMarine* cur = vlc->getUnit(i);
-        cur->battleCry();
-        cur->rangedAttack();
-        cur->meleeAttack();
-    }
-    delete vlc;
-
-    return 0;
-} */
 
 int	main() {
 
@@ -44,22 +24,13 @@ int	main() {
 
 		squad->push(bob);
 		squad->push(jim);
-		squad->push(bob); // this will not add him and display an error, as he already exists in the squad
-		squad->push(jim); // this will not add him and display an error, as he already exists in the squad
+		squad->push(bob);
+		squad->push(jim);
 
         for (int i = 0; squad->getUnit(i); i++)
             std::cout << i << std::endl;
 	std::cout << std::endl;
 
-	/*
-	** Uncomment the part below if needed to test with bigger squads (will be longer to read though)
-	*/
-	// std::cout << "1.5 Pushing two new marines and two new terminators in Squad1" << std::endl;
-	// 	squad->push(new TacticalMarine);
-	// 	squad->push(new TacticalMarine);
-	// 	squad->push(new AssaultTerminator);
-	// 	squad->push(new AssaultTerminator);
-	// std::cout << std::endl;
 
 	std::cout << "2. Creating constructor copy of Squad1 in an ISquad" << std::endl;
 		ISquad *isquad = new Squad(*squad);
