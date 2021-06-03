64a91aea0a684601656e75a7364175de949376c0
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-06-03 19:05:11 +0200

    default commit message

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -17,9 +17,14 @@ void	sum(int &a){
 	a++;
 }
 
+void	les(char &c){
+	c--;
+}
+
 int main(){
 
 	int array[] = {0,1,2,3,4,5};
+	char list[] = {'b', 'c', 'd'};
 
 	for (int i = 0; i < 6; i++)
 		std::cout << array[i];
@@ -30,4 +35,16 @@ int main(){
 	for (int i = 0; i < 6; i++)
 		std::cout << array[i];
 	std::cout << std::endl;
+
+	for (int i = 0; i < 3; i++)
+		std::cout << list[i];
+	std::cout << std::endl;
+
+	iter(list, 3, les);
+
+	for (int i = 0; i < 3; i++)
+		std::cout << list[i];
+	std::cout << std::endl;
+
+	return 0;
 }
