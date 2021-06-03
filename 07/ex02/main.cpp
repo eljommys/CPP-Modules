7cf52a33e6ceae2a7fef7ab08f967107d572613e
j53rran0 <jserrano.gonzalez.moreno@gmail.com>
2021-06-03 19:14:29 +0200

    default commit message

--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -17,6 +17,10 @@ int main(){
 	Array<int> b(7);
 	Array<int> c(b);
 
+	Array<char> e;
+	Array<char> f(7);
+	Array<char> g(f);
+
 	for (int i = 0; i < 7; i++)
 		b[i] = i * 2;
 
@@ -38,5 +42,28 @@ int main(){
 
 	for (int i = 0; i < 8; i++)
 		std::cout << d[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < 7; i++)
+		f[i] = i * 2 + (int)'a';
+
+	for (int i = 0; i < 7; i++)
+		std::cout << f[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < 8; i++)
+		std::cout << g[i] << " ";
+	std::cout << std::endl;
+
+	e = f;
+
+	for (int i = 0; i < 8; i++)
+		std::cout << e[i] << " ";
+	std::cout << std::endl;
+
+	Array<char> h(e);
+
+	for (int i = 0; i < 8; i++)
+		std::cout << h[i] << " ";
 	std::cout << std::endl;
 }
